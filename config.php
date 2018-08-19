d0c02841b5401fd40a824e97775708317135a25f
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-08-19 13:14:25 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *  Core site settings
+ */
+define( 'SETTINGS',		'data/site.json' );
+
+/**
+ *  Database location
+ */
+define( 'DATA',		'data/site.db' );
+
+/**
+ *  This setting is only needed once during setup
+ */
+define( 'DEFAULT_USER',	'admin' );
+
+/**
+ *  Remember to enable write permissions to site.json and site.db. 
+ *  If you intend to run the /data/backup.sh script, also enable write 
+ *  permissions to the /data/backups folder
+ */
+
+/**
+ *  Theme directory
+ */
+define( 'THEME_DIR',		'themes/' );
+
+/**
+ *  Set true for sites behind Tor
+ */
+define( 'SKIP_LOCAL',		true );
+
+
+
+
+
+
+// Do not edit the following lines
+if ( !defined('INCLUDED') ) { 
+	die(); 
+}
+
